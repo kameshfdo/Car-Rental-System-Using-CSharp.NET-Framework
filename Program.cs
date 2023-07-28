82531571d0f28d111202aff3f7f3bf28c738e8df
kamesh fernando <kameshkumara921@gmail.com>
2023-07-28 22:19:26 +0530

    add login UI

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Car_Rental_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
